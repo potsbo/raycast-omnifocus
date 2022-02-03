735dbd1a9792de85a5122d4734b24252c105ca03
Shimpei Otsubo <potsbo@gmail.com>
2022-02-03 23:35:13 +0900

    Cleaner look with icons

--- src/perspectives.tsx
+++ src/perspectives.tsx
@@ -1,15 +1,10 @@
-import { ActionPanel, Detail, List, PushAction, useNavigation } from "@raycast/api";
+import { ActionPanel, Icon, List, useNavigation } from "@raycast/api";
 import { useEffect, useState } from "react";
-import { omniFunc, getTasksForPerspective } from "@jacobx1/of-sdk";
+import { omniFunc, getTasksForPerspective, completeTaskById, Task } from "@jacobx1/of-sdk";
 
 interface Perspective {
   title: string;
   tasksPromise: Promise<Task[]>;
-}
-
-interface Task {
-  id: string;
-  name: string;
 }
 
 interface TaskListProps {
@@ -18,7 +13,7 @@ interface TaskListProps {
 
 const TaskList = (props: TaskListProps) => {
   const { tasks } = props;
-  const [loadedTasks, setTasks] = useState<Task[]>([]);
+  const [loadedTasks, setTasks] = useState<Task[]>();
   useEffect(() => {
     const resolve = async () => {
       const resolved = await tasks;
@@ -27,14 +22,30 @@ const TaskList = (props: TaskListProps) => {
     resolve();
   });
   return (
-    <List>
-      {loadedTasks.map((t) => (
+    <List isLoading={loadedTasks === undefined}>
+      {loadedTasks?.map((t) => (
         <List.Item
           title={t.name}
           key={t.id}
+          icon={t.completed ? Icon.Checkmark : Icon.Circle}
+          accessoryIcon={Icon.ArrowClockwise}
           actions={
             <ActionPanel>
-              <PushAction title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
+              <ActionPanel.Item
+                title="Mark as Completed"
+                onAction={() => {
+                  setTasks((prev) => {
+                    if (prev === undefined) {
+                      return;
+                    }
+                    const updated = [...prev];
+                    const idx = updated.findIndex((p) => t.id === p.id);
+                    updated[idx].completed = !updated[idx].completed;
+                    return updated;
+                  });
+                  completeTaskById(t.id);
+                }}
+              />
             </ActionPanel>
           }
         />
@@ -47,9 +58,41 @@ export const getPerspectives = omniFunc(function () {
   return this.Perspective.all;
 }, {});
 
+const getIconForPerspective = (name: string) => {
+  switch (name) {
+    case "Inbox": {
+      return Icon.Envelope;
+    }
+    case "Projects": {
+      return Icon.Desktop;
+    }
+    case "Tags": {
+      return Icon.Pin;
+    }
+    case "Forecast": {
+      return Icon.Calendar;
+    }
+    case "Flagged": {
+      return Icon.Star;
+    }
+    case "Review": {
+      return Icon.Hammer;
+    }
+    default: {
+      return Icon.Document;
+    }
+  }
+};
+
+const defaultPerspectives = ["Inbox", "Projects", "Tags", "Forecast", "Flagged", "Review"];
+
+const fromName = (name: string) => {
+  return { title: name, tasksPromise: getTasksForPerspective(name) };
+};
+
 export default function Command() {
   const { push } = useNavigation();
-  const [perspectives, setPerspectives] = useState<Perspective[]>();
+  const [perspectives, setPerspectives] = useState<Perspective[]>(defaultPerspectives.map((n) => fromName(n)));
   useEffect(() => {
     const loadPerspectives = async () => {
       const perspectives: ({ name: string } | null)[] = await getPerspectives();
@@ -68,11 +111,12 @@ export default function Command() {
   }, []);
 
   return (
-    <List isLoading={perspectives === undefined}>
-      {perspectives?.map((p) => (
+    <List>
+      {perspectives.map((p) => (
         <List.Item
           title={p.title}
           key={p.title}
+          icon={getIconForPerspective(p.title)}
           actions={
             <ActionPanel>
               <ActionPanel.Item title="Push" onAction={() => push(<TaskList tasks={p.tasksPromise} />)} />
