b5e0e732ea23a2f0de238a45127836408362eafa
Shimpei Otsubo <potsbo@gmail.com>
2022-02-05 21:21:03 +0900

    Fix types

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,10 +3,10 @@ import { getProjects } from "../api";
 import { useLoad } from "../utils";
 
 export const ProjectList = () => {
-  const projects = useLoad(getProjects);
+  const { value: projects, isLoading } = useLoad(getProjects);
 
   return (
-    <List isLoading={projects === undefined}>
+    <List isLoading={isLoading}>
       {projects?.map((p) => (
         <List.Item title={p.name} key={p.id} icon={p.completed ? Icon.Checkmark : Icon.Circle} />
       ))}
