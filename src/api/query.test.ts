d15950ac404f113828e7679a45fc0dbb31de3156
Shimpei Otsubo <potsbo@gmail.com>
2022-02-09 03:00:14 +0900

    Add new test

--- src/api/query.test.ts
+++ src/api/query.test.ts
@@ -1,14 +1,19 @@
 import { buildExecutionContext, ExecutionContext } from "graphql/execution/execute";
-import { GetInboxTasksDocument, GetTasksDocument } from "./generated/graphql";
+import { GetInboxTasksDocument, GetTasksDocument, GetTasksInProjectDocument } from "./generated/graphql";
 import { loadSchemaSync } from "@graphql-tools/load";
 import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
 import { join } from "path";
 import { genQuery } from "./query";
 import prettier from "prettier";
+import { GraphQLError } from "graphql";
 
 const schema = loadSchemaSync(join(__dirname, "..", "..", "assets", "schema.graphql"), {
   loaders: [new GraphQLFileLoader()],
 });
+
+const validateExecontext = (obj: ExecutionContext | readonly GraphQLError[]): obj is ExecutionContext => {
+  return "operation" in obj;
+};
 
 test("query for GetTasksDocument", () => {
   const document = GetTasksDocument;
@@ -26,6 +31,20 @@ test("query for GetInboxTasksDocument", () => {
     schema: schema,
     document: document,
   }) as ExecutionContext;
+
+  expect(prettier.format(genQuery("parent", exeContext))).toMatchSnapshot();
+});
+
+test("query for GetTasksInProjectDocument", () => {
+  const document = GetTasksInProjectDocument;
+  const exeContext = buildExecutionContext({
+    schema: schema,
+    document: document,
+    variableValues: { projectId: "foobar" },
+  });
+  if (!validateExecontext(exeContext)) {
+    fail();
+  }
 
   expect(prettier.format(genQuery("parent", exeContext))).toMatchSnapshot();
 });
