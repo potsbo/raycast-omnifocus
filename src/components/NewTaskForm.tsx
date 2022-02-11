b4f916ab443a3ccb417dfe27c12b750de0f5fcb6
Shimpei Otsubo <potsbo@gmail.com>
2022-02-12 07:28:42 +0900

    Use graphql

--- src/components/NewTaskForm.tsx
+++ src/components/NewTaskForm.tsx
@@ -1,14 +1,7 @@
 import { ActionPanel, Form, SubmitFormAction, Icon, showToast, ToastStyle } from "@raycast/api";
 import { useMemo } from "react";
-import { createNewTask, getNestedTags } from "../api";
-import { get } from "../api/fetch";
-import { useLoad } from "../utils";
-
-const getProjectsAndTags = async () => {
-  const tags = await getNestedTags();
-  const folders = await get("GetNestedProjects", {}).then((r) => r.defaultDocument.folders.edges.map((e) => e.node));
-  return { tags, folders };
-};
+import { createNewTask } from "../api";
+import { useQuery } from "../api/fetch";
 
 interface Props {
   defaultProject: string;
@@ -23,9 +16,9 @@ interface FormFields {
 }
 
 export const NewTaskForm = ({ defaultProject, defaultTags }: Props) => {
-  const tp = useLoad(getProjectsAndTags, "NewTask:TagsAndProjects");
-  const tags = tp.value?.tags;
-  const folders = tp.value?.folders;
+  const tp = useQuery("GetTaskCreationSupportInfo");
+  const tags = tp.value?.defaultDocument.tags.edges.map((e) => e.node);
+  const folders = tp.value?.defaultDocument.folders.edges.map((e) => e.node);
 
   const tagOptions = useMemo<JSX.Element[]>(() => {
     if (tags === undefined) {
@@ -33,9 +26,11 @@ export const NewTaskForm = ({ defaultProject, defaultTags }: Props) => {
     }
     const opts: JSX.Element[] = [];
     tags.forEach((f) => {
-      f.tags.forEach((t) => {
-        opts.push(<Form.TagPicker.Item key={t.id} value={t.id} title={`${f.tagName}: ${t.name}`} icon={Icon.Pin} />);
-      });
+      f.tags.edges
+        .map((e) => e.node)
+        .forEach((t) => {
+          opts.push(<Form.TagPicker.Item key={t.id} value={t.id} title={`${f.name}: ${t.name}`} icon={Icon.Pin} />);
+        });
     });
     return opts;
   }, [tags]);
@@ -50,9 +45,7 @@ export const NewTaskForm = ({ defaultProject, defaultTags }: Props) => {
       f.projects.edges
         .map((e) => e.node)
         .forEach((t) => {
-          opts.push(
-            <Form.Dropdown.Item key={t.id} value={t.id} title={`${f.name}: ${t.name}`} icon={Icon.List} />
-          );
+          opts.push(<Form.Dropdown.Item key={t.id} value={t.id} title={`${f.name}: ${t.name}`} icon={Icon.List} />);
         });
     });
     return opts;
