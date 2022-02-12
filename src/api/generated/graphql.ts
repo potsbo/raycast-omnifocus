0cbb123e01a6caebc5b3c12defe6c42319062a47
Shimpei Otsubo <potsbo@gmail.com>
2022-02-12 21:42:55 +0900

    Support complex whose conditions

--- src/api/generated/graphql.ts
+++ src/api/generated/graphql.ts
@@ -544,7 +544,7 @@ export const GetTasksDocument = gql`
 export const GetInboxTasksDocument = gql`
     query GetInboxTasks {
   defaultDocument {
-    inboxTasks @whose(condition: {field: "effectivelyCompleted", value: "false"}) {
+    inboxTasks @whose(condition: {operation: "_and", children: [{field: "effectivelyCompleted", value: "false"}]}) {
       edges {
         node {
           ...TaskViewModel
--- src/api/whose.ts
+++ src/api/whose.ts
@@ -1,4 +1,4 @@
-import { FieldNode, Kind, ObjectValueNode } from "graphql";
+import { FieldNode, Kind, ObjectValueNode, ValueNode } from "graphql";
 
 const MATCH_OPERATORS = [
   "_equals",
@@ -12,17 +12,7 @@ const MATCH_OPERATORS = [
   "_match",
 ] as const;
 
-const LOGICAL_OPERATORS = [
-  "_equals",
-  "_contains",
-  "_beginsWith",
-  "_endsWith",
-  "_greaterThan",
-  "_greaterThanEquals",
-  "_lessThan",
-  "_lessThanEquals",
-  "_match",
-] as const;
+const LOGICAL_OPERATORS = ["_and", "_or", "_not"] as const;
 
 type MatchOperator = typeof MATCH_OPERATORS[number];
 type LogicalOperator = typeof LOGICAL_OPERATORS[number];
@@ -33,11 +23,13 @@ type Condition =
       field: string;
       operator: MatchOperator;
       value: string;
+      enabled: boolean;
     }
   | {
       kind: "LOGICAL";
       operator: LogicalOperator;
       children: Condition[];
+      enabled: boolean;
     };
 
 const getMatchOperator = (op: string): MatchOperator | null => {
@@ -58,6 +50,16 @@ const getMatchOperator = (op: string): MatchOperator | null => {
       return "_lessThan";
     case "<=":
       return "_lessThanEquals";
+  }
+  return null;
+};
+
+const getLogicalOperator = (op: string): LogicalOperator | null => {
+  if (LOGICAL_OPERATORS.indexOf(op as LogicalOperator) > -1) {
+    return op as LogicalOperator;
+  }
+  if (LOGICAL_OPERATORS.indexOf(`_${op}` as LogicalOperator) > -1) {
+    return `_${op}` as LogicalOperator;
   }
   return null;
 };
@@ -92,21 +94,12 @@ const mustExtractStringArg = (c: ObjectValueNode, key: string, defaultValue?: st
   return node.value;
 };
 
-export const extractCondition = (f: FieldNode): Condition | null => {
-  const d = f.directives?.find((d) => d.name.value === "whose");
-  if (!d) {
-    return null;
-  }
-  const condition = d.arguments?.find((a) => a.name.value === "condition")?.value;
-  if (!condition || condition.kind !== Kind.OBJECT) {
+const extractConditionFromValueNode = (condition: ValueNode): Condition => {
+  if (condition.kind !== Kind.OBJECT) {
     throw new Error("malformed conditions");
   }
 
   const enabled = mustExtractBoolArg(condition, "enabled", true);
-  if (!enabled) {
-    return null;
-  }
-
   const operator = mustExtractStringArg(condition, "operation", "=");
   const matchOp = getMatchOperator(operator);
   if (matchOp) {
@@ -117,19 +110,90 @@ export const extractCondition = (f: FieldNode): Condition | null => {
       field,
       operator: matchOp,
       value,
+      enabled,
     };
   }
 
-  // TODO: logical filter
-  return null;
+  const logicalOp = getLogicalOperator(operator);
+  if (logicalOp) {
+    const childrenNode = condition.fields.find((f) => f.name.value === "children")?.value;
+    if (!childrenNode || childrenNode.kind !== Kind.LIST) {
+      throw new Error("malformed conditions");
+    }
+
+    return {
+      kind: "LOGICAL",
+      operator: logicalOp,
+      children: childrenNode.values.map((f) => {
+        if (f.kind !== Kind.OBJECT) {
+          throw new Error("malformed conditions");
+        }
+        return extractConditionFromValueNode(f);
+      }),
+      enabled,
+    };
+  }
+
+  throw new Error(`Unknown operator ${operator}`);
+};
+
+export const extractCondition = (f: FieldNode): Condition | null => {
+  const d = f.directives?.find((d) => d.name.value === "whose");
+  if (!d) {
+    return null;
+  }
+  const condition = d.arguments?.find((a) => a.name.value === "condition")?.value;
+  if (!condition) {
+    throw new Error("malformed conditions");
+  }
+
+  return extractConditionFromValueNode(condition);
+};
+
+const filterPresent = <T>(x: T | null): x is T => {
+  return x !== null && x !== undefined;
+};
+
+const reduce = (c: Condition | null): Condition | null => {
+  if (c === null) {
+    return null;
+  }
+  if (!c.enabled) {
+    return null;
+  }
+  if (c.kind === "MATCH") {
+    return c;
+  }
+
+  const reducedChildren = c.children.map(reduce).filter(filterPresent);
+  if (reducedChildren.length === 0) {
+    return null;
+  }
+  switch (c.operator) {
+    case "_not":
+      return { ...c, children: reducedChildren };
+    case "_and":
+    case "_or":
+      if (reducedChildren.length === 1) {
+        return reducedChildren[0];
+      }
+      return { ...c, children: reducedChildren };
+  }
+};
+
+const compileCondition = (whose: Condition): string => {
+  if (whose.kind === "LOGICAL") {
+    const children = `[${whose.children.map(compileCondition).join(",")}]`;
+    return `{ ${whose.operator}: ${children}}`;
+  }
+  return `{ ${whose.field}: { ${whose.operator}: ${JSON.stringify(whose.value)}}}`;
 };
 
 export const compileWhoseParam = (whose: Condition | null): string => {
-  if (whose === null) {
+  const reduced = reduce(whose);
+  if (reduced === null) {
     return "";
   }
-  if (whose.kind === "LOGICAL") {
-    throw new Error("Not supported");
-  }
-  return `.whose({ ${whose.field}: { ${whose.operator}: ${JSON.stringify(whose.value)}}})`;
+
+  return `.whose(${compileCondition(reduced)})`;
 };
