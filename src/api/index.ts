8683291cddd6896084e2c742dec045a9086e8439
Shimpei Otsubo <potsbo@gmail.com>
2022-02-15 23:54:10 +0900

    Inject runner

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,59 +1,52 @@
 import { run } from "@jxa/run";
 import { GraphQLResolveInfo } from "graphql";
-import { QueryResolvers, Resolvers } from "./generated/graphql";
+import { Resolvers } from "./generated/graphql";
 import { genQuery } from "./query";
 import camelCase from "camelcase";
 
-// The rootValue provides a resolver function for each API endpoint
-const rootValue: QueryResolvers = {
-  application: (_: unknown, _2: unknown, info: GraphQLResolveInfo) => {
-    const q = genQuery("t", "OmniFocus", info);
+export const buildRootValue = (runner: <T>(fn: (_: T) => unknown, args: T) => unknown) => {
+  return new Proxy(
+    {},
+    {
+      get: function (_, name: string) {
+        if (name === "application") {
+          return (_: unknown, _2: unknown, info: GraphQLResolveInfo) => {
+            const q = genQuery("t", "OmniFocus", info);
 
-    const fn = (arg: { q: string }) => {
-      return eval(arg.q);
-    };
+            const fn = (arg: { q: string }) => {
+              return eval(arg.q);
+            };
 
-    return run(fn, { q });
-  },
-};
+            return runner(fn, { q });
+          };
+        }
 
-const push = (typeName: string, props: unknown, info: GraphQLResolveInfo) => {
-  const fieldName = camelCase(typeName);
-  const q = genQuery("t", "OmniFocus", info);
-  const mut = `
-  const app = Application("OmniFocus");
-  const obj = app.${typeName}(${JSON.stringify(props)});
-  app.defaultDocument.${fieldName}.push(obj);
-`;
+        return (typeName: string, props: unknown, info: GraphQLResolveInfo) => {
+          const fieldName = camelCase(typeName);
+          const q = genQuery("t", "OmniFocus", info);
+          const mut = `
+          const app = Application("OmniFocus");
+          const obj = app.${typeName}(${JSON.stringify(props)});
+          app.defaultDocument.${fieldName}.push(obj);
+        `;
 
-  const fn = (arg: { mut: string; q: string }) => {
-    eval(mut);
+          const fn = (arg: { mut: string; q: string }) => {
+            eval(mut);
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const t = Application("OmniFocus").defaultDocument;
-    return eval(arg.q);
-  };
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const t = Application("OmniFocus").defaultDocument;
+            return eval(arg.q);
+          };
 
-  return run(fn, { props: JSON.stringify(props), q });
-};
-
-const query = new Proxy(rootValue, {
-  get: function (target, name: string) {
-    if (name === "application") {
-      return target[name];
+          return runner(fn, { mut, q });
+        };
+      },
     }
-
-    return function (props: Record<string, any>, _2: unknown, info: GraphQLResolveInfo) {
-      const { returnType } = info;
-      // TODO correctly extract return type
-      const typeName = (returnType as any).ofType.name;
-      push(typeName, props, info);
-    };
-  },
-});
+  );
+};
 
 export const resolvers: Resolvers = {
-  Query: query,
+  Query: buildRootValue(run),
 };
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
