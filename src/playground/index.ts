7897f266c379a428a2e4e55b5e39d9f9ec78986d
Shimpei Otsubo <potsbo@gmail.com>
2022-02-21 22:58:37 +0900

    Switch to graphiql

--- src/playground/index.ts
+++ src/playground/index.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { graphqlHTTP } from "express-graphql";
-import expressPlayground from "graphql-playground-middleware-express";
 import { build } from "../api/sdef-to-schema";
 import { printSchema } from "graphql";
 import { buildRootValue } from "../api/rootValue";
@@ -18,8 +17,7 @@ build(appPath).then((schema) => {
       const app = express();
       const appName = basename(appPath).split(".").slice(0, -1).join(".");
       const rootValue = buildRootValue(appName, runJXACode);
-      app.use("/graphql", graphqlHTTP({ schema, rootValue }));
-      app.use("/playground", expressPlayground({ endpoint: "/graphql" }));
+      app.use("/graphql", graphqlHTTP({ schema, rootValue, graphiql: true }));
       app.listen(4000);
       console.log("Running a GraphQL API server at http://localhost:4000/graphql");
     }
